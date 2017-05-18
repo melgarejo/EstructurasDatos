62a461b9315ef75badfa13c063971401382d7edd
Sebastian Melgarejo <seba.melgarejo87@gmail.com>
2017-05-17 21:41:13 -0300

    Add files via upload

---
+++ Estructuras_de_datos/Ej04_01.vb
@@ -0,0 +1,28 @@
+﻿
+'4.1. Elaborar un algoritmo que permita cargar 5 números en un arreglo de 5 elementos; calcular la
+'media y luego imprimir cada elemento del arreglo y la desviación que tiene respecto a la media.
+'La MEDIA se obtiene la sumatoria de los todos elementos del arreglo y se divide por la cantidad
+'de los mismos. La DESVIACION de cada elemento se obtiene restándole al elemento la
+'MEDIA
+
+Module Ej04_01
+
+    Sub Main()
+        Dim arreglo(4) As Integer
+        Dim suma As Decimal
+        Dim media As Decimal
+        Dim desviacion As Decimal
+        For i = 0 To arreglo.Count - 1
+            Console.Write("Ingrese el " & i + 1 & "º número: ")
+            arreglo(i) = Console.ReadLine()
+            suma = suma + arreglo(i)
+            media = suma / (i + 1)
+            desviacion = arreglo(i) - media
+            Console.WriteLine(i + 1 & "º elemento: " & arreglo(i))
+            Console.WriteLine("Suma hasta el " & i + 1 & "º elemento: " & suma)
+            Console.WriteLine("Media hasta el " & i + 1 & "º elemento: " & media)
+            Console.WriteLine("Desviación del " & i + 1 & "º elemento: " & desviacion)
+        Next
+        Console.ReadKey()
+    End Sub
+End Module
---
+++ Estructuras_de_datos/Ej04_02.vb
@@ -0,0 +1,46 @@
+﻿
+'4.2. Se tiene tres arrays cargados programáticamente con una lista de productos: el primero tiene el
+'código, el segundo el nombre y el tercero el precio unitario. Repetitivamente se ingresa un
+'código de producto y el programa muestra su descripción y precio, luego el usuario ingresa la
+'cantidad con lo que el programa responde calculando el total del producto y el total general
+'(acumulando los productos anteriores). Esto sucede hasta que el código sea cero
+
+Module Ej04_02
+    Sub main()
+        Dim codProd() = New Short() {1, 2, 3}
+        Dim nomProd() = New String() {"coca", "fanta", "manaos"}
+        Dim precProd() = New Single() {45.4, 32.22, 23.99}
+        Dim cod As Short
+        cod = leerCodigo("Ingrese el código de producto o cero (0) para terminar")
+        Console.WriteLine("El producto es:" & buscarCodigo(cod, codProd))
+        Console.ReadKey()
+    End Sub
+
+    Private Function leerCodigo(mensaje As String) As Short
+        Dim valor As Short
+        Do
+            Console.WriteLine(mensaje)
+            valor = Console.ReadLine
+        Loop Until validar(valor)
+        Return valor
+    End Function
+
+    Private Function validar(valor As Short) As Boolean
+        If valor = 0 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Function buscarCodigo(codig As Short, CodigoProd() As Short) As Integer
+        Dim aux As Integer
+        For i = 0 To CodigoProd.Length - 1
+            If codig = CodigoProd(i) Then
+                Return aux
+            End If
+        Next
+
+    End Function
+
+End Module
